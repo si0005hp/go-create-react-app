bf71c30cf718a9215f49318707ce77a12b5a6e79
si0005hp <si0005hp@gmail.com>
2019-05-06 03:09:35 +0900

    init

---
+++ server/data.go
@@ -0,0 +1,38 @@
+package server
+
+import "github.com/si0005hp/go-create-react-app/webpack"
+
+// User represents current user session
+type User struct {
+	Email     string
+	FirstName string
+	LastName  string
+}
+
+// ViewData contains data for the view
+type ViewData struct {
+	CurrentUser  User
+	assetsMapper webpack.AssetsMapper
+}
+
+// NewViewData creates new data for the view
+func NewViewData(buildPath string) (ViewData, error) {
+	assetsMapper, err := webpack.NewAssetsMapper(buildPath)
+	if err != nil {
+		return ViewData{}, err
+	}
+
+	return ViewData{
+		CurrentUser: User{
+			Email:     "bill@example.com",
+			FirstName: "Bill",
+			LastName:  "Black",
+		},
+		assetsMapper: assetsMapper,
+	}, nil
+}
+
+// Webpack maps file name to path
+func (d ViewData) Webpack(file string) string {
+	return d.assetsMapper(file)
+}
